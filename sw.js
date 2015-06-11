411607a488f7b16cff92ef0067d78b212feedbec
Francisco Jordano <fjordano@mozilla.com>
2015-06-11 15:33:33 +0100

    First commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+importScripts('/bower_components/serviceworkerware/dist/sww.js');
+
+var worker = new ServiceWorkerWare();
+
+// Just for logging purposes
+worker.use(function(req, res) {
+  console.log('SWW:: Requesting url ' + req.url);
+  return null;
+});
+
+// Claim!
+worker.use({
+  onActivate: function(e) {
+    e.waitUntil(self.clients.claim());
+  }
+});
+
+worker.init();
